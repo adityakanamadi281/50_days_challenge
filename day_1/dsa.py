a12d52716cc3800d41b9aa492d06ead5fc3dafbe
adityakanamadis@gamil.com <adityakanamadis@gmail.com>
2025-11-09 23:45:41 +0530

    09/11/2025

---
+++ day_1/dsa.py
@@ -0,0 +1,13 @@
+# 1. Data Structures (List, Strings, Sets, Tuples, Dictionary)
+# OOP's 
+# List
+# Tuples
+# Dictionary
+# sets
+# Stacks
+# Queues
+
+my_list = [1,2,3,4,5,6,7,8,9]
+my_list.append(10)
+my_list.pop()
+print(my_list)
