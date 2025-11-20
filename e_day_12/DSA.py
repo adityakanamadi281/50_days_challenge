be7d79e9a861d4a6b2ae51a0541e3d1e62962d3b
adityakanamadis@gamil.com <adityakanamadis@gmail.com>
2025-11-20 13:33:38 +0530

    Sorting

---
+++ e_day_12/DSA.py
@@ -0,0 +1,18 @@
+# Sorting
+
+# 1. Selection Sort 
+
+
+nums = [5, 7, 8, 4, 1, 6, 9, 2]
+
+def selection_sort(nums):
+    n = len(nums)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if nums[j] < nums[min_index]:
+                min_index = j
+        nums[i], nums[min_index] = nums[min_index], nums[i]
+    return nums
+
+print(selection_sort(nums))
