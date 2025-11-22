caaf78740ca074e0b6add052ea18adff5dc3a45f
adityakanamadis@gamil.com <adityakanamadis@gmail.com>
2025-11-22 23:39:36 +0530

    22/11/2025

--- e_day_14/Python.py
+++ e_day_14/Python.py
@@ -27,3 +27,55 @@ class Rectangle:
 r = Rectangle(10, 5)
 print("Area:", r.area())
 print("Perimeter:", r.perimeter())
+
+
+class BankAccount:
+    def __init__(self, acc_no, balance=0):
+        self.acc_no = acc_no
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+        else:
+            print("Insufficient Balance")
+
+    def check_balance(self):
+        print("Balance:", self.balance)
+
+a = BankAccount(101, 5000)
+a.deposit(2000)
+a.withdraw(3000)
+a.check_balance()
+
+
+
+
+class Car:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+
+    def display(self):
+        print(f"{self.brand} {self.model} ({self.year})")
+
+car1 = Car("Tesla", "Model 3", 2024)
+car1.display()
+
+
+
+
+class Temperature:
+    def c_to_f(self, c):
+        return (c * 9/5) + 32
+
+    def f_to_c(self, f):
+        return (f - 32) * 5/9
+
+t = Temperature()
+print("25°C in F:", t.c_to_f(25))
+print("68°F in C:", t.f_to_c(68))
