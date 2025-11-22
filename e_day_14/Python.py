dbe97d2203cdfd7a884fd5888bd22003dcf3c05f
adityakanamadis@gamil.com <adityakanamadis@gmail.com>
2025-11-22 23:35:34 +0530

    OOP's

---
+++ e_day_14/Python.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+    def display(self):
+        print(f"Name: {self.name}, Age: {self.age}, Grade: {self.grade}")
+
+s1 = Student("Aditya", 20, "A")
+s1.display()
